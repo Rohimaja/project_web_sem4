a329b6a756cc1bba7ff0512d76a726283f8e1c0a
edwink29 <edwn.k29@gmail.com>
2025-05-28 11:13:19 +0700

    seeder dan factory tahun ajaran dan ruangan

---
+++ database/factories/TahunAjaranFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TahunAjaranFactory extends Factory
+{
+    public function definition(): array
+    {
+        $tahunAwal = $this->faker->numberBetween(2020, 2025);
+        $tahunAkhir = $tahunAwal + 1;
+
+        return [
+            'tahun_awal' => $tahunAwal,
+            'tahun_akhir' => $tahunAkhir,
+            'keterangan' => $this->faker->randomElement([
+                'Semester Ganjil', 'Semester Genap', 'Tahun Ajaran Baru', 'Semester Pendek'
+            ]),
+            'status' => $this->faker->boolean ? 1 : 0, // 1 = aktif, 0 = nonaktif
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}
